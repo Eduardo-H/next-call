ad95817658159414be77cc1fe100de572b5d41f2
Eduardo-H <edu-henrique2014@hotmail.com>
2023-05-09 13:00:33 -0300

    Connect calendar page implemented

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/router'
+import { signIn, useSession } from 'next-auth/react'
+import { ArrowRight, Check } from 'phosphor-react'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+
+import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { Container, Header } from '../styles'
+
+export default function Register() {
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    signIn('google')
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Connect your calendar!</Heading>
+        <Text>
+          Connect your calendar to automatically check busy hours and new events
+          as they are scheduled.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Connected <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Connect <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+
+        {hasAuthError && (
+          <AuthError size="sm">
+            We {"weren't"} able to connect to your Google Calendar, make sure
+            that you enabled the necessary permissions.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
+          Next step <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
